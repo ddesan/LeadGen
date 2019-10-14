55ce635cd710193e101a5993bb74bd7270034f6d
Temme <ddesantiago0@gmail.com>
2019-10-14 14:57:34 -0500

    update

--- node_modules/react-with-styles-interface-aphrodite/src/aphroditeInterface.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, css, flushToStyleTag } from 'aphrodite';
-import aphroditeInterfaceFactory from './aphroditeInterfaceFactory';
-
-export default aphroditeInterfaceFactory({
-  StyleSheet,
-  css,
-  flushToStyleTag,
-});
