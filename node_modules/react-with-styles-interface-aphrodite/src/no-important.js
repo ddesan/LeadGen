22b440eabb34906a1a08ae407f72aa712b968233
juanto121 <juanf.torres.lopez@gmail.com>
2020-02-22 12:28:22 -0500

    Removes cached node_mods

--- node_modules/react-with-styles-interface-aphrodite/src/no-important.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, css, flushToStyleTag } from 'aphrodite/no-important';
-import aphroditeInterfaceFactory from './aphroditeInterfaceFactory';
-
-export default aphroditeInterfaceFactory({
-  StyleSheet,
-  css,
-  flushToStyleTag,
-});
